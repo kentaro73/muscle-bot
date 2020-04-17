bb02cab2567a0dc9f2e93d9e4ea5fc4148a580df
kentaro73 <k.uneune0703@gmail.com>
2020-04-18 07:37:28 +0900

    fixed typo

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,55 +1,118 @@
-class LinebotController < ApplicationController
-  require 'line/bot'
-  require 'open-uri'
 
-  protect_from_forgery :except => [:callback]
-
-  def callback
-    body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
-
-    events = client.parse_events_from(body)
-    events.each { |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          input = event.message['text']
-          case input
-          when /.*(終わった|おわった|おわた|できた|終了|完了|したよ|した).*/
-            word = ["ないすで〜す", "ナイス！", "いい感じ！", "ナイスバルク！", "キレてるよ！", "バリバリ！", "仕上がってるよ！", "はい！ずどーん！", "腹筋グレネード！","肩メロン！", "背中に羽が生えてる！", "脚が歩いてる！"].sample
-            push = "#{word}"
-          else
-            push = "テキスト以外はわからないよ〜"
-          end
-          message = {
-            type: 'text',
-            text: push
-          }
-          client.reply_message(event['replyToken'], message)
-
-        when Line::Bot::Event::Follow
-          line_id = event['source']['userId']
-          User.create(line_id: line_id)
-        when Line::Bot::Event::Unfollow
-          line_id = event['source']['userId']
-          User.find_by(line_id: line_id).destroy
-        end
+  class LinebotController < ApplicationController
+    require 'line/bot' # gem 'line-bot-api'
+    require 'open-uri' 
+    require 'kconv' 
+    require 'rexml/document' 
+  
+    # callbackアクションのCSRFトークン認証を無効
+    protect_from_forgery :except => [:callback]
+  
+    def callback
+      body = request.body.read
+      signature = request.env['HTTP_X_LINE_SIGNATURE']
+      unless client.validate_signature(body, signature)
+        error 400 do 'Bad Request' end
       end
-      }
-      head :ok
+      events = client.parse_events_from(body)
+      events.each { |event| 
+        case event
+          # メッセージが送信された場合の対応
+        when Line::Bot::Event::Message
+          case event.type
+            # ユーザーからテキスト形式のメッセージが送られてきた場合
+          when Line::Bot::Event::MessageType::Text
+            # event.message['text']:ユーザーから送られたメッセージ
+            input = event.message['text']
+            url = "https://www.drk7.jp/weather/xml/13.xml"
+            xml = open( url ).read.toutf8
+            doc = REXML::Document.new(xml)
+            xpath = 'weatherforecast/pref/area[4]/'
+            # 当日朝のメッセージの送信の下限値は20％としているが、明日・明後日雨が降るかどうかの下限値は30％としている
+            min_per = 30
+            case input
+              # 「明日」or「あした」というワードが含まれる場合
+            when /.*(明日|あした).*/
+              # info[2]:明日の天気
+              per06to12 = doc.elements[xpath + 'info[2]/rainfallchance/period[2]'].text
+              per12to18 = doc.elements[xpath + 'info[2]/rainfallchance/period[3]'].text
+              per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
+              if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+                push =
+                  "明日の天気だよね。\n明日は雨が降りそうだよ(>_<)\n今のところ降水確率はこんな感じだよ。\n  6〜12時 #{per06to12}％\n 12〜18時  #{per12to18}％\n 18〜24時 #{per18to24}％\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
+              else
+                push = 
+                  "明日の天気？\n明日は雨は降らない予定だよ(^^)\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
+              end
+            when /.*(明後日|あさって).*/
+              per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]'].text
+              per12to18 = doc.elements[xpath + 'info[3]/rainfallchance/period[3]'].text
+              per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]'].text
+              if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+                push =
+                  "明後日の天気だよね。\n何かあるのかな？\n明後日は雨が降りそう...\n当日の朝に雨が降りそうだったら教えるからね！"
+              else
+                push = 
+                  "明後日の天気？\n気が早いねー！何かあるのかな？\n明後日は雨は降らない予定だよ(^^)\nまた当日の朝の最新の天気予報で雨が降りそうだったら教えるからね！"
+              end
+            when /.*(終わった|おわった|おわた|できた|終了|完了|したよ|した).*/
+              word = ["ないすで〜す", "ナイス！", "いい感じ！", "ナイスバルク！", "キレてるよ！", "バリバリ！", "仕上がってるよ！", "はい！ずどーん！", "腹筋グレネード！","肩メロン！", "背中に羽が生えてる！", "脚が歩いてる！"].sample
+              push = "#{word}"
+            when /.*(こんにちは|こんばんは|初めまして|はじめまして|おはよう).*/
+              push = 
+                "こんにちは。\n声をかけてくれてありがとう\n今日があなたにとっていい日になりますように(^^)"
+            else
+              per06to12 = doc.elements[xpath + 'info/rainfallchance/period[2]'].text
+              per12to18 = doc.elements[xpath + 'info/rainfallchance/period[3]'].text
+              per18to24 = doc.elements[xpath + 'info/rainfallchance/period[4]'].text
+              if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+                word = 
+                  ["雨だけど元気出していこうね！",
+                    "雨に負けずファイト！",
+                    "雨だけどあなたの明るさでみんなを元気にしてあげて(^^)"].sample
+                push = 
+                  "今日の天気？\n今日は雨が降りそうだから傘があったほうが安心だよ。\n  6〜12時 #{per06to12}％\n 12〜18時  #{per12to18}％\n 18〜24時 #{per18to24}％\n#{word}"
+              else
+                word = 
+                  ["天気もいいから一駅歩いてみるのはどう？(^^)",
+                   "今日会う人のいいところを見つけて是非その人に教えてあげて(^^)",
+                   "素晴らしい一日になりますように(^^)",
+                   " 雨が降っちゃったらごめんね(><)"].sample
+                push = 
+                  "今日の天気？\n今日は雨は降らなさそうだよ。\n#{word}"
+                end
+              end
+              # テキスト以外（画像等）のメッセージが送られた場合
+            else
+              push = "テキスト以外はわからないよ〜(;;)"
+            end
+            message = {
+              type: 'text',
+              text: push
+            }
+            client.reply_message(event['replyToken'], message)
+            # LINEお友達追加された場合
+          when Line::Bot::Event::Follow
+            # 登録したユーザーのidをユーザーテーブルに格納
+            line_id = event['source']['userId']
+            User.create(line_id: line_id)
+            # LINEお友達解除された場合
+          when Line::Bot::Event::Unfollow
+            # お友達解除したユーザーのデータをユーザーテーブルから削除
+            line_id = event['source']['userId']
+            User.find_by(line_id: line_id).destroy
+          end
+        }
+        head :ok
     end
-
-  private 
-
+  
+    private 
+  
     def client
       @client ||= Line::Bot::Client.new { |config|
         config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
         config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
       }
     end
-end
+  end
+   
