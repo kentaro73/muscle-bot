b5db2aca9bfed042e62bbff82bbc4db6b5b50339
kentaro73 <k.uneune0703@gmail.com>
2020-04-17 22:43:49 +0900

    finished basic

---
+++ app/controllers/linebot_controller.rb
@@ -0,0 +1,50 @@
+class LinebotController < ApplicationController
+  require 'line/bot'
+
+  protect_form_forgery :except => [:callback]
+
+  def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      return head :bad_request
+    end
+    events = client.parse_events_form(body)
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          case input
+          when /.*(終わった|おわった|おわた|できた|終了|完了|したよ|した).*/
+            word = ["ないすで〜す", "ナイス！", "いい感じ！", "ナイスバルク！", "キレてるよ！", "バリバリ！", "仕上がってるよ！", "はい！ずどーん！", "腹筋グレネード！","肩メロン！", "背中に羽が生えてる！", "脚が歩いてる！"].sample
+            push = "#{word}"
+          end
+        else
+          push = "テキスト以外はわからないよ〜"
+        end
+        message = {
+          type: 'text',
+          text: push
+        }
+        client.reply_message(event['replyToken'], message)
+      when Line::Bot::Event::Follow
+        line_id = event['source']['userId']
+        User.create(line_id: line_id)
+      when Line::Bot::Event::Unfollow
+        line_id = event['source']['userId']
+        User.find_by(line_id: line_id).destroy
+      end
+      }
+      head :ok
+  end
+
+  private 
+
+    def client
+      @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
+    end
+end
